fff7ea8e355d752737dba1abeb3ce35ae0e9040e
Raz Ali <raz.ali@slalom.com>
2020-04-11 22:55:08 -0400

    Add Dynamodb table for BuckleyConfiguration

--- handler.js
+++ handler.js
@@ -1,9 +1,10 @@
 const {
-  graphql,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLNonNull,
+    graphql,
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLNonNull,
 } = require('graphql');
 
 const AWS = require('aws-sdk');
@@ -11,74 +12,99 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const promisify = foo => new Promise((resolve, reject) => {
-  foo((error, result) => {
-    if (error) {
-      reject(error);
-    } else {
-      resolve(result);
-    }
-  });
+    foo((error, result) => {
+        if (error) {
+            reject(error);
+        } else {
+            resolve(result);
+        }
+    });
 });
 
+const getProjectRepos = name => promisify(callback =>
+    dynamoDb.get({
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {name},
+    }, callback))
+    .then((result) => {
+        if (!result.Item) {
+            return name;
+        }
+        return result.Item.repos;
+    })
+    .then(name => `Hello, ${name}.`);
+
 const getGreeting = firstName => promisify(callback =>
-  dynamoDb.get({
-    TableName: process.env.DYNAMODB_TABLE,
-    Key: { firstName },
-  }, callback))
-  .then((result) => {
-    if (!result.Item) {
-      return firstName;
-    }
-    return result.Item.nickname;
-  })
-  .then(name => `Hello, ${name}.`);
+    dynamoDb.get({
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {firstName},
+    }, callback))
+    .then((result) => {
+        if (!result.Item) {
+            return firstName;
+        }
+        return result.Item.nickname;
+    })
+    .then(name => `Hello, ${name}.`);
 
 const changeNickname = (firstName, nickname) => promisify(callback =>
-  dynamoDb.update({
-    TableName: process.env.DYNAMODB_TABLE,
-    Key: { firstName },
-    UpdateExpression: 'SET nickname = :nickname',
-    ExpressionAttributeValues: {
-      ':nickname': nickname,
-    },
-  }, callback))
-  .then(() => nickname);
+    dynamoDb.update({
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {firstName},
+        UpdateExpression: 'SET nickname = :nickname',
+        ExpressionAttributeValues: {
+            ':nickname': nickname,
+        },
+    }, callback))
+    .then(() => nickname);
+
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: {type: new GraphQLNonNull(GraphQLString)},
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        repos: {type: GraphQLList}
+    })
+});
+
+const getProject = name => promisify(callback => {
+    dynamoDb.get({
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {name},
+    }, callback))
+});
 
 const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType', // an arbitrary name
-    fields: {
-      // the query has a field called 'greeting'
-      greeting: {
-        // we need to know the user's name to greet them
-        args: { firstName: { name: 'firstName', type: new GraphQLNonNull(GraphQLString) } },
-        // the greeting message is a string
-        type: GraphQLString,
-        // resolve to a greeting message
-        resolve: (parent, args) => getGreeting(args.firstName),
-      },
-    },
-  }),
-  mutation: new GraphQLObjectType({
-    name: 'RootMutationType', // an arbitrary name
-    fields: {
-      changeNickname: {
-        args: {
-          firstName: { name: 'firstName', type: new GraphQLNonNull(GraphQLString) },
-          nickname: { name: 'nickname', type: new GraphQLNonNull(GraphQLString) },
+    query: new GraphQLObjectType({
+        name: 'RootQueryType', // an arbitrary name
+        fields: {
+            project: {
+                args: {name: {name: 'name', type: new GraphQLNonNull(GraphQLString)}},
+                type: ProjectType,
+                resolve: (parent, args) => getProject(args.name),
+            },
         },
-        type: GraphQLString,
-        resolve: (parent, args) => changeNickname(args.firstName, args.nickname),
-      },
-    },
-  }),
+    }),
+    mutation: new GraphQLObjectType({
+        name: 'RootMutationType', // an arbitrary name
+        fields: {
+            changeNickname: {
+                args: {
+                    firstName: {name: 'firstName', type: new GraphQLNonNull(GraphQLString)},
+                    nickname: {name: 'nickname', type: new GraphQLNonNull(GraphQLString)},
+                },
+                type: GraphQLString,
+                resolve: (parent, args) => changeNickname(args.firstName, args.nickname),
+            },
+        },
+    }),
 });
 
 // We want to make a GET request with ?query=<graphql query>
 // The event properties are specific to AWS. Other providers will differ.
 module.exports.query = (event, context, callback) =>
-  graphql(schema, event.queryStringParameters.query)
-  .then(
-    result => callback(null, { statusCode: 200, body: JSON.stringify(result) }),
-    err => callback(err)
-  );
+    graphql(schema, event.queryStringParameters.query)
+        .then(
+            result => callback(null, {statusCode: 200, body: JSON.stringify(result)}),
+            err => callback(err)
+        );
